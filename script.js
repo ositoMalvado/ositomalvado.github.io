e4d3767c6ca2dcaa8af1795521613a4579f78b6b
Julián Perez <mordecaaii@gmail.com>
2024-02-25 22:34:45 -0300

    Initial commit

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 ﻿
+index = 1
+const url = `https://pokeapi.co/api/v2/pokemon/${index}`;
 
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Pokemon:', data);
+    audioUrl = data.cries.legacy;
+		imagen = data.sprites.front_default;
+		imagenShiny = data.sprites.front_shiny;
+  })
+  .catch(error => {
+    console.error('There was a problem with your fetch operation:', error);
+  });
