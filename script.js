6105ab66dbad102c22d1c89ad350323a2a481737
Julián Perez <mordecaaii@gmail.com>
2024-02-27 23:00:03 -0300

    copy

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-﻿
-index = 1
-const url = `https://pokeapi.co/api/v2/pokemon/${index}`;
+﻿const emailElement = document.getElementById('link-cont');
+emailElement.style.display = 'inline-block';
+function copyToClipboard() {
+  const emailElement = document.getElementById('link-cont');
+  const emailText = emailElement.innerText;
 
-fetch(url)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log('Pokemon:', data);
-    audioUrl = data.cries.legacy;
-		imagen = data.sprites.front_default;
-		imagenShiny = data.sprites.front_shiny;
-  })
-  .catch(error => {
-    console.error('There was a problem with your fetch operation:', error);
-  });
+  const textarea = document.createElement('textarea');
+  textarea.value = emailText;
+  document.body.appendChild(textarea);
+
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+
+  // Crear un mensaje de alerta moderno
+  const alertElement = document.createElement('div');
+  alertElement.textContent = 'Mail copiado!';
+  alertElement.classList.add(
+      'fixed',
+      'top-1/2',
+      'left-1/2',
+      '-translate-x-1/2',
+      '-translate-y-1/2',
+      'transform',
+      'bg-black',
+      'bg-opacity-30',
+      'text-white',
+      'text-lg',
+      'font-semibold',
+      'px-6',
+      'py-3',
+      'rounded-lg',
+      'shadow-lg',
+      'z-50',
+      'transition-all',
+      'duration-300'
+  );
+  document.body.appendChild(alertElement);
+
+  // Ajustar el tiempo de desaparición del mensaje
+  setTimeout(() => {
+      alertElement.style.opacity = '0';
+      setTimeout(() => {
+          alertElement.remove();
+      }, 300); // Desvanecer el mensaje después de 300 milisegundos
+  }, 2000); // Mantener el mensaje durante 2 segundos
+}
